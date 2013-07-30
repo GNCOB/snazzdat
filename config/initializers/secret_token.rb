6bb8529d7d502994092edec69f8f54d25ee38c19
sanarchy7 <nankrah007@gmail.com>
2013-07-29 23:46:54 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snazzdat::Application.config.secret_key_base = '939c6da9a4e9c2c79d7c7b407da668f7e9f84687542f1b7ab2f7bb8656709dfd3214c4dc0469608b52a9cf6c19c26dba351b1ad3da49db9dee0da94a584fd1d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Snazzdat::Application.config.session_store :cookie_store, key: '_snazzdat_session'
