08ddf3be0007ab5a85dacfc5b9cb8da5bdfd5962
sanarchy7 <nankrah007@gmail.com>
2013-11-06 23:01:08 -0500

    update retailigence api version; fix privacy doc;

--- config/initializers/retailigence.rb
+++ config/initializers/retailigence.rb
@@ -1,10 +1,10 @@
   Retailigence.configure do |config|
     if Rails.env.production?
       API_KEY = '9ueu6I9fHzu-jXzNzTt2toBXBVBBstxr'
-      BASE_URI = 'http://api.retailigence.com/v2.0'
+      BASE_URI = 'http://api.retailigence.com/v2.1'
     else
       API_KEY = 'aIzdHVPfx5e20bav-Lx7xtdP7MK7usst'
-      BASE_URI = 'http://apitest.retailigence.com/v2.0'
+      BASE_URI = 'http://apitest.retailigence.com/v2.1'
     end
 
     config.api_key = API_KEY
