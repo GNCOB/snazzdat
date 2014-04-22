a159e658386f9c21d5f5857bda704b27bafd08c6
sanarchy7 <nankrah007@gmail.com>
2014-04-22 00:15:34 -0400

    store names based on location

---
+++ lib/retailer_blacklist.rb
@@ -0,0 +1,11 @@
+module RetailerBlacklist
+  COSTCO = {code: 1, name: 'Cost Plus World Market'}
+  WALGREENS = {code: 2, name: 'Walgreens'}
+  LOWES = {code: 3, name: 'Lowes'}
+  POTTERY_BARN = {code:4, name: 'Pottery Barn'}
+  BESTBUY_MOBILE = {code: 5, name: 'Best Buy Mobile'}
+  RITE_AID = {code: 6, name: 'Rite Aid'}
+  VITAMIN_SHOPPE = {code: 7, name: 'Vitamin Shoppe'}
+  BED_BATH_BEYOND = {code: 8, name: 'Bed Bath and Beyond'}
+  STAPLES = {code: 9, name: 'Staples'}
+end
